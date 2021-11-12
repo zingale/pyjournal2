50f5e9746d4858f781e744fc91aab7f90f4afa02
Michael Zingale <michael.zingale@stonybrook.edu>
2021-11-11 19:29:20 -0500

    update mathjax macros

--- pyjournal2/sphinx_base/source/conf.py
+++ pyjournal2/sphinx_base/source/conf.py
@@ -116,17 +116,19 @@ html_context = {
 
 
 # -- Options for MathJax
-mathjax_config = {'TeX': {'Macros': {}}}
+# -- Options for MathJax
+mathjax3_config = {'tex': {'macros': {}}}
 
 with open('mathsymbols.tex', 'r') as f:
     for line in f:
         macros = re.findall(r'\\newcommand{\\(.*?)}(\[(\d)\])?{(.+)}', line)
         for macro in macros:
             if len(macro[1]) == 0:
-                mathjax_config['TeX']['Macros'][macro[0]] = "{"+macro[3]+"}"
+                mathjax3_config['tex']['macros'][macro[0]
+                                                ] = "{" + macro[3] + "}"
             else:
-                mathjax_config['TeX']['Macros'][macro[0]] = ["{"+macro[3]+"}", int(macro[2])]
-
+                mathjax3_config['tex']['macros'][macro[0]] = [
+                    "{" + macro[3] + "}", int(macro[2])]
 
 
 # -- Options for HTMLHelp output ---------------------------------------------
